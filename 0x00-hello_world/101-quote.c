ca511fe2e834939166104de0da49026a22530fd8
antwanola <antwanola29@gmail.com>
2023-07-14 09:15:15 +0000

    making quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-/**
- * The comment betty has been waiting
- * for
- */
+/* The comment betty has been waiting for */
+
 int main(int param)
 {
 /*Assigning value to param*/
 param = 1;
 /*printing to std error*/
-fprintf(stderr, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n");
 /*returning assigned param*/
 return (param);
 }
