6add9b4f7b35e2b71ffed317acf990bf4ed0feda
antwanola <antwanola29@gmail.com>
2023-07-14 01:39:53 +0000

    using betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,7 @@
 
 int main(void)
 {
-
-	char myString [] = "Programming is like building a multilingual puzzle";
-		puts(myString);
-	return (0);
-
+char myString[] = "Programming is like building a multilingual puzzle";
+puts(myString);
+return (0);
 }
