4bbf89901c0bc46debc68f026b9504bed9fc27b6
antwanola <antwanola29@gmail.com>
2023-07-14 08:56:36 +0000

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 printf("Size of a char: %zu byte(s)\n", (sizeof(char)));
 printf("Size of an int: %d byte(s)\n", (sizeof(int)));
 printf("Size of a long int: %li byte(s)\n", (sizeof(signed long int)));
-printf("Size of a long long int: %i byte(s)\n", (sizeof(long long int)));
+printf("Size of a long long int:%i byte(s)\n", (sizeof(long long int)));
 printf("Size of a float: %lu byte(s)\n", (sizeof(float)));
 printf("\n");
 return (0);
