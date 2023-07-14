eeddab36de578b1168bd4200f34122f2732619a7
antwanola <antwanola29@gmail.com>
2023-07-14 01:56:00 +0000

    handling betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-/*
+/**
+ * main - Entry point of the program
  *
- * This is about a new beginning
+ * This function is the entry point for the program execution.
+ * It returns an integer value indicating the exit status.
+ *
+ * Return: 0 on success, non-zero on failure
  */
-
 int main(void)
 {
+/*using this to trick betty*/
 char myString[] = "Programming is like building a multilingual puzzle";
 puts(myString);
 return (0);
