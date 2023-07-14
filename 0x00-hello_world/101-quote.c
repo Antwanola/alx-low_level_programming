05cd4ff99ecc7bacee80564987242a6869fef66c
antwanola <antwanola29@gmail.com>
2023-07-14 09:40:36 +0000

    making comment

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
-/* The comment betty has been waiting for */
-
+/**
+*main - Entry point
+*Description: 'making commenter'
+*Return: Always 1 (Success)
+*/
 int main(void)
 {
-/*Assigning value to param*/
 int param = 1;
-/*printing to std error*/
 write(2,
 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 /*returning assigned param*/
