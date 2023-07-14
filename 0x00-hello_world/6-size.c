bbd5d535aa8d45ea45c1a863b9fe61eb0db60dff
antwanola <antwanola29@gmail.com>
2023-07-14 03:51:26 +0000

    okay now

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point of the program
+ *
+ * This function is the entry point for the program execution.
+ * It returns an integer value indicating the exit status.
+ *
+ * Return: 0 on success, non-zero on failure
+ */
+int main(void)
+{
+/* Your code here */
+printf("Size of a char:%c byte(s)\n", (sizeof(char)));
+printf("Size of an int:%d byte(s)\n", (sizeof(int)));
+printf("Size of a long int:%li byte(s)\n", (sizeof(signed long int)));
+printf("Size of a long long int:%i byte(s)\n", (sizeof(long long int)));
+printf("Size of a float:%lu byte(s)\n", (sizeof(float)));
+return (0);
+}
+
