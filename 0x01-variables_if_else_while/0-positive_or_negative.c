50e2ece2431da884ccd6d6cf4ac332cd5d4b343b
antwanola <antwanola29@gmail.com>
2023-07-14 23:23:56 +0000

    Printing number to know if positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - This is the entry point
+ * Description: A program printing rand num
+ * Return: 0 (Success)
+ * /
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is possitive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
