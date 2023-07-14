0128952ab8b0d768f823c37576b973a6d1d5aac2
antwanola <antwanola29@gmail.com>
2023-07-14 02:10:25 +0000

    Handling bettygit add .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 /*using this to trick betty*/
-char myString[] = "Programming is like building a multilingual puzzle";
+char myString[] = "\"Programming is like building a multilingual puzzle";
 puts(myString);
 return (0);
 }
