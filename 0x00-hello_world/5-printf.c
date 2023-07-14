8146b0d197f9171ed5ad63c8dd6fbfea86e1fd6b
antwanola <antwanola29@gmail.com>
2023-07-14 01:59:42 +0000

    Handling bettygit add .

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * This function is the entry point for the program execution.
+ * It returns an integer value indicating the exit status.
+ *
+ * Return: 0 on success, non-zero on failure
+ */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
