1cc4ae405c77712d798b5440cd24c03ea439ed62
antwanola <antwanola29@gmail.com>
2023-07-14 09:24:27 +0000

    making quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@ int main(void)
 /*Assigning value to param*/
 int param = 1;
 /*printing to std error*/
-fprintf(stderr, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n");
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 /*returning assigned param*/
 return (param);
 }
