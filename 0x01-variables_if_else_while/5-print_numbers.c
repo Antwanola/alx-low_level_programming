407feed4f7ff3ec0eeb99cc4a6368ca523eba3aa
antwanola <antwanola29@gmail.com>
2023-07-15 02:00:42 +0000

    priting numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry function
+ * Description: using just putchar to print all the alphabets
+ * Return: 0 (success)
+ */
+int main(void)
+{
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
+
