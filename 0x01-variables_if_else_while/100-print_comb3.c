a8a0e83666af3df27c9c30b7741fbb30e9f192df
antwanola <antwanola29@gmail.com>
2023-07-15 14:58:58 +0000

    combo3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,9 @@ int main(void)
 	int num1;
 	int num2;
 
-	for (num1 = 48; num1 <= 55; num1++)
+	for (num1 = 48; num1 <= 56; num1++)
 	{
-	for (num2 = num1 + 1; num2 <= 56; num2++)
+	for (num2 = num1 + 1; num2 <= 57; num2++)
 	{
 	putchar(num1);
 	putchar(num2);
