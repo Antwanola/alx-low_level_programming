269e7f3385884bc149f70084f49a142e6d7de5a5
antwanola <antwanola29@gmail.com>
2023-07-15 02:54:03 +0000

    spaced nums

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,9 +8,11 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9 ; num++)
 	{
 	putchar(num + '0');
+		if (num == 9)
+		continue;
 	putchar(',');
 	putchar(' ');
 	}
