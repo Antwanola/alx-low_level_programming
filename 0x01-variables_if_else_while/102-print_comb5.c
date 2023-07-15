58245678dc038a619b3c665bd7bf0ed7c80e9a4b
antwanola <antwanola29@gmail.com>
2023-07-15 16:16:06 +0000

    hello

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,8 +20,8 @@ int main(void)
 		putchar((num2 / 10) + '0');
 		putchar((num2 % 10) + '0');
 
-	if (num1 == 98 && num2 == 99)
-		continue;
+		if (num1 == 98 && num2 == 99)
+			continue;
 		putchar(',');
 		putchar(' ');
 	}
