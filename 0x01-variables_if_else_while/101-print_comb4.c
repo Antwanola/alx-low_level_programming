a29904ab3363666919503eb7424dfcaf2713c8e6
antwanola <antwanola29@gmail.com>
2023-07-15 07:53:17 +0000

    adjusted but not right yet

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 /**
  * main - entry function
  * Description: using just putchar to print all the alphabets
- * Return: 0 (success)
+ * Return: 0 (success)	
  */
 int main(void)
 {
@@ -10,16 +10,16 @@ int main(void)
 	int num2;
 	int num3;
 
-	for (num1 = 0; num1 <= 10; num1++)
+	for (num1 = 48; num1 < 56; num1++)
 	{
-	for (num2 = num2 + 1; num2 <= 11; num2++)
+	for (num2 = num2 + 1; num2 <57 ; num2++)
 	{
-	for (num3 = num2 + 1; num3 <= 12; num3++)
+	for (num3 = num2 + 1; num3 < 58; num3++)
 	{
 	putchar((num1 % 10) + '0');
 	putchar((num2 % 10) + '0');
 	putchar((num3 % 10) + '0');
-	if (num1 == 9 && num2 == 10 && num3 == 11)
+	if (num1 == 47 && num2 == 56 && num3 == 57)
 		continue;
 	putchar(',');
 	putchar(' ');
