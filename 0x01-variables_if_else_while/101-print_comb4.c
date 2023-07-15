d738e5b8089a989b44e973244760e80b38865c60
antwanola <antwanola29@gmail.com>
2023-07-15 15:19:41 +0000

    the combo 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,11 @@ int main(void)
 	int num2;
 	int num3;
 
-	for (num1 = 0; num1 <= 7; num1++)
+	for (num1 = 0; num1 < 8; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 8 ; num2++)
+		for (num2 = num1 + 1; num2 < 9; num2++)
 	{
-		for (num3 = num2 + 1; num3 <= 9; num3++)
+		for (num3 = num2 + 1; num3 < 10; num3++)
 	{
 		putchar((num1 % 10) + '0');
 		putchar((num2 % 10) + '0');
