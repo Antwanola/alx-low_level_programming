cf5b20e77793f4500a24dcec1bfb0509a4ffb007
antwanola <antwanola29@gmail.com>
2023-07-15 00:47:17 +0000

    making last number talk

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - This is the entry point
+ * Description: A program printing rand num
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int mod;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n != 0)
+	{
+			mod = n % 10;
+			if (mod > 5)
+			{
+			printf("Last digit of %d is %d and is greater than %d\n", n, mod, mod);
+			}
+			else if (mod == 0)
+			{
+			printf("Last digit of %d is %d and is 0\n", n, mod);
+			}
+			else if (mod < 6 && mod != 0)
+			{
+			printf("Last digit of %d is %d and is less that 6 and not 0\n", n, mod);
+			return (0);
+			}
+	}
+	else
+	printf("%d is zero\n", n);
+	return (0);
+}
