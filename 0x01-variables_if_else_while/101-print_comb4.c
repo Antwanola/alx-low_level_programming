dd21d22edefd8d8677438d6614a8cfde0222346f
antwanola <antwanola29@gmail.com>
2023-07-15 07:24:56 +0000

    making numbers combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,16 +10,17 @@ int main(void)
 	int num2;
 	int num3;
 
-	for (num1 = 0; num1 <= 9; num1++)
+	for (num1 = 0; num1 <= 10; num1++)
 	{
-	for (num2 = num2 + 1; num2 <= 10; num2++)
+	for (num2 = num2 + 1; num2 <= 11; num2++)
 	{
-	for (num3 = num2 + 1; num3 <= 11; num3++)
+	for (num3 = num2 + 1; num3 <= 12; num3++)
 	{
 	putchar((num1 % 10) + '0');
 	putchar((num2 % 10) + '0');
 	putchar((num3 % 10) + '0');
-	if (num1 != 8 || num2 != 9 || num3 != 10)
+	if (num1 == 9 && num2 == 10 && num3 == 11)
+		continue;
 	putchar(',');
 	putchar(' ');
 	}
