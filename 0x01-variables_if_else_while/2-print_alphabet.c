949c7430d885d7141172e651f6a225907fe3d333
antwanola <antwanola29@gmail.com>
2023-07-15 01:03:08 +0000

    using putchar to print all alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry function
+ * Description: using just putchar to print all the alphabets
+ * Return: 0 (success)
+ */
+int main() {
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++) {
+        putchar(letter);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
+
