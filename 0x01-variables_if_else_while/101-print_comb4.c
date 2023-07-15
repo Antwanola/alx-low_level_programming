5d23e6bfb647fdaf1dddfd1b4deae022b3c2b8f0
antwanola <antwanola29@gmail.com>
2023-07-15 15:10:16 +0000

    away with problems

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,16 +10,16 @@ int main(void)
 	int num2;
 	int num3;
 
-	for (num1 = 48; num1 <= 54; num1++)
+	for (num1 = 0; num1 <= 7; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 55 ; num2++)
+		for (num2 = num1 + 1; num2 <= 8 ; num2++)
 	{
-		for (num3 = num2 + 1; num3 <= 56; num3++)
+		for (num3 = num2 + 1; num3 <= 9; num3++)
 	{
-		putchar(num1);
-		putchar(num2);
-		putchar(num3);
-	if (num1 != 55 || num2 != 56 || num3 != 57)
+		putchar((num1 % 10) + '0');
+		putchar((num2 % 10) + '0');
+		putchar((num3 % 10) + '0');
+	if (num1 != 7 || num2 != 8 || num3 != 9)
 	putchar(',');
 	putchar(' ');
 	}
