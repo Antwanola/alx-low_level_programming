af91ec7aa31921fe123f51b64ae806157f21e274
antwanola <antwanola29@gmail.com>
2023-07-15 00:48:49 +0000

    making last number talk

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 			mod = n % 10;
 			if (mod > 5)
 			{
-			printf("Last digit of %d is %d and is greater than %d\n", n, mod, mod);
+			printf("Last digit of %d is %d and is greater than 5\n", n, mod, mod);
 			}
 			else if (mod == 0)
 			{
