8e96a780ad4db01934d5d42715b66cb8cbc956ad
antwanola <antwanola29@gmail.com>
2023-07-18 17:39:45 +0000

    adding alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.c"
+
+/**
+ * main - The usual main entry point of all c program
+ *
+ * Description: Just printing _puctchar
+ *
+ * Return: 0 as usual
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
