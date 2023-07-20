b7c1cba0cad9fc229a6d63d02070be21a0de7cb3
antwanola <antwanola29@gmail.com>
2023-07-20 16:47:11 +0000

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _isupper - function to take char in and determine if its a uppercase or not
+ *
+ * Description: takes in vals and know whether upper or not
+ *
+ * @c: the input
+ *
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		_putchar(c);
+		_putchar(':');
+		_putchar(' ');
+		return (1);
+	}
+	else if (c >= 'a' && c <= 'Z')
+	{
+		_putchar(c);
+		_putchar(':');
+		_putchar(' ');
+		return (0);
+	}
+
+	return (0);
+}
