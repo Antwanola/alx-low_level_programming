54a908f0558b62abae52ed0e4cf13dd398af6866
antwanola <antwanola29@gmail.com>
2023-07-20 16:58:02 +0000

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -13,17 +13,7 @@
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
-		_putchar(c);
-		_putchar(':');
-		_putchar(' ');
 		return (1);
-	}
 	else
-	{
-		_putchar(c);
-		_putchar(':');
-		_putchar(' ');
 		return (0);
-	}
 }
