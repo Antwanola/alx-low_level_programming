d16b24e3262a4f714163db61625ae6495278606d
antwanola <antwanola29@gmail.com>
2023-07-24 20:03:43 +0000

    second str reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string -  entry point
+ * @s: string goes here
+ *
+ * Return void
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	char *temp;
+
+	for (i = 0; i >= 0; i--)
+	{
+		temp = *s[i];
+
+		*s = temp;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,3 +10,4 @@ int _strlen(char *s);
 void _puts(char *str);
 
 void print_rev(char *s);
+void rev_string(char *s);
