8019b09e8e5acf41e069b6cbdadc93470fdffbb7
antwanola <antwanola29@gmail.com>
2023-07-24 22:44:54 +0000

    string print

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+#include <string.h>
+
+
+void puts2(char *str)
+{
+	for (i = 0; i < strlen(str); i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <unistd.h>
 
+
 int _putchar(char c);void reset_to_98(int *n);
-
 void swap_int(int *a, int *b);
-
 int _strlen(char *s);
 void _puts(char *str);
-
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
+
+
+#endif
