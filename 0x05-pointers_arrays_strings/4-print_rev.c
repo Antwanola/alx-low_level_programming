316e92655b02be434914eced4474940937566485
antwanola <antwanola29@gmail.com>
2023-07-24 19:46:41 +0000

    reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,13 @@ void print_rev(char *s)
 {
 	int i, len, temp;
 
-	len = strlen(s);
+	len = strlen(*s);
 
 	for (i = 0; i < len / 2; i++)
 	{
-		temp  = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
+		temp  = *s[i];
+		*s[i] = *s[len - i - 1];
+		*s[len - i - 1] = temp;
 	}
 	_putchar('\n');
 }
