33be34a347a49a533a96fde70d0a1376b811c17f
antwanola <antwanola29@gmail.com>
2023-07-24 15:44:29 +0000

    prints strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - prints out a string
+ * @str: gets the string var
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	size_t length = strlen(str);
+
+	for (let i = 0; i <= length; i++)
+		_putchar(i);
+}
