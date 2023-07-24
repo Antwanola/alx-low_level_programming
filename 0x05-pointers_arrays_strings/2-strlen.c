24adce8e3771f443f417922068e333493bf5d81b
antwanola <antwanola29@gmail.com>
2023-07-24 15:24:31 +0000

    returns string len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: the string goes here
+ *
+ * Return: string len
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
