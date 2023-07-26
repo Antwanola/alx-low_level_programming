5470218f55e841516cf5ded45c39b2aef638e7d6
antwanola <antwanola29@gmail.com>
2023-07-26 17:58:24 +0000

    concatenate string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,21 @@
 #include "main.h"
 #include <string.h>
+/**
+ * _strcat - Concatenates strings
+ * @dest: destination string
+ * @src: source string
+ * Return: char
+ */
 
 char *_strcat(char *dest, char *src)
 {
-	return strcat(dest, src);
+	int len = strlen(dest);
+	int len2 = strlen(src);
+	int i;
+
+	for (i = 0; i < len2; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+char *_strcat(char *dest, char *src);
+
+
+#endif
