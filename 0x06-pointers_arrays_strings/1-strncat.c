e43e65857447a69965e7913c0c0bfcfc43ec550b
antwanola <antwanola29@gmail.com>
2023-07-26 19:11:10 +0000

    concat string with certain number

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 	int len = strlen(dest);
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[len + i] = src[i];
 	}
