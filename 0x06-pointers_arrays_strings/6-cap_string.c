7993111733d23c6b1318245a46174d9a3e5e349b
antwanola <antwanola29@gmail.com>
2023-07-27 20:31:57 +0000

    turn all first letters to uppercase

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,27 @@
 #include "main.h"
-
+/**
+ * cap_string - prints 1st letters as upper
+ * Description: Takes a pointer str and conversts first letter to upper
+ * @q: takes string pointers
+ *
+ * Return: char
+ */
 
 char *cap_string(char *q)
 {
-	int i;
+int i;
 
-	while (q[i] != '\0')
-	{
-		if (q[i] >= 'A' && q[i] <= 'Z')
-		{
-			q[i] = q[i] -32;
-		}
-		i++;
-	}
+while (q[i] != '\0')
+{
+if (q[i] >= 'A' && q[i] <= 'Z')
+	q[i];
+if (q[i - 1] == ' ' || q[i - 1] == '\n' || q[i - 1] == '\t' || q[i - 1] == '.')
+{
+while (q[i] >= 'a' && q[i] <= 'z')
+q[i] = q[i] - 32;
+}
+i++;
+}
 
-	return (q);
+return (q);
 }
