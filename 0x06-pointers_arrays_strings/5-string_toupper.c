ba78970c94c2ed1413932e0f73afba13ed401c7d
antwanola <antwanola29@gmail.com>
2023-07-27 00:12:46 +0000

    converts any lowercase letter to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * string_toupper - converts and lower case to upper
+ * Description: Upper case comverter
+ * @s: String
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+
+	}
+	return (s);
+}
