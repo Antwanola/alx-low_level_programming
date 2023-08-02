5c1a5aea4468189921abe7d4147ba5daccf1f2d6
antwanola <antwanola29@gmail.com>
2023-08-02 00:06:05 +0000

    string matcher

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * _strpbrk - returns pointer to the matching string
+ * Description: Gets string and matching string and returns pointer
+ * @s: Input string
+ * @accept: check preference string
+ * Return: pointer char
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
