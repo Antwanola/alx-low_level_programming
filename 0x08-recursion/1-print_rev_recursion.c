e1a7d26ee1fd7b04b5ae5ab3a1dbcee31217d951
antwanola <antwanola29@gmail.com>
2023-08-03 00:00:22 +0000

    print rev recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * _print_rev_recursion - Takes a tring and return the reverse
+ * Description: Takes a string and return rev
+ * @s: Input string
+ * Return: Void
+ */
 
 void _print_rev_recursion(char *s)
 {
