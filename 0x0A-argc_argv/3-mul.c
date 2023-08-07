1c11df1bdc09434c5f3811ec76ec5dbd11e77ebc
antwanola <antwanola29@gmail.com>
2023-08-07 16:27:12 +0000

    Argv values multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,11 +5,16 @@ int main(int argc, char **argv)
 {
 	int i;
 
-	if (argv[1])
+	if (argv[1] && argv[2])
 	{
 		int first = atoi(argv[1]);
 		int second = atoi(argv[2]);
 		printf("%d\n", first * second);
+		return (0);
 	}
-	return (0);
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
