55071ff4189b304f5ff675200812a7e9a615d027
antwanola <antwanola29@gmail.com>
2023-08-09 23:53:29 +0000

    grid using alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = (int **)malloc(sizeof(int) * height);
+	grid =malloc(sizeof(int *) * height);
 	if (grid == NULL)
 		return (NULL);
 
