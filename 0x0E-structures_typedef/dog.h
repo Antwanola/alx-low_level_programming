e21f6b349712307138d3e0cdddce76c3b3e7e9c7
antwanola <antwanola29@gmail.com>
2023-08-14 23:10:13 +0000

    struct declaration

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,13 @@
+
+/**
+ * struct dog - dog struct
+ * @name: name pointer
+ * @age: age value(float)
+ * @owner: owner pointer
+ *
+ * Description: Longer description
+ */
+
 #ifndef DOG_H
 #define DOG_H
 struct dog 
