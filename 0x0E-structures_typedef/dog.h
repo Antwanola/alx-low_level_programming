fa7516191001410774ed778ced619e5cff36f24d
antwanola <antwanola29@gmail.com>
2023-08-14 23:17:39 +0000

    struct declaration

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,10 @@
-
-/**
- * struct dog - dog struct
- * @name: name pointer
- * @age: age value(float)
- * @owner: owner pointer
- *
- * Description: Longer description
- */
-
 #ifndef DOG_H
 #define DOG_H
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
-	char *owner
+	char *owner;
 };
 
 #endif
