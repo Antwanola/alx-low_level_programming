bf2e7466e1fbcacd34b0b4c98510cb7ede1c7d1e
antwanola <antwanola29@gmail.com>
2023-08-15 23:04:56 +0000

    new dog typedef function

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,15 +11,15 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-	if (!d->name)
+	if (!(d->name))
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (!d->age)
+	if (!(d->age))
 		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
-	if (!d->owner)
+	if (!(d->owner))
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
