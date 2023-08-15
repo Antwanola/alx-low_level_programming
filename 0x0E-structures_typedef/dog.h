d1d2bec3b094e22d9f787f24a83474773f53a21b
antwanola <antwanola29@gmail.com>
2023-08-15 20:44:05 +0000

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - structure for dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: A dog structure
+ */
+
 struct dog
 {
 	char *name;
