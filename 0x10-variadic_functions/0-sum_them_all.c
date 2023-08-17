e43aba3a107a0d04ed42f17385d2fd42a0bfa0ac
antwanola <antwanola29@gmail.com>
2023-08-17 11:28:20 +0000

    Variadic function count

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+
+/**
+ * sum_them_all - Sum all numbers in argument
+ * @n: the arg count
+ * @...: infinit args
+ * Return: int
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+	va_list vars;
+	int i = 0, sum = 0;
+	
+	if (n == 0)
+		return (0);
+
+	va_start (vars, n);
+
+	while (i < n)
+	{
+		sum += va_arg(vars, int);
+		i++;
+	}
+	va_end(vars);
+	return (sum);
+
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+*Variadic Functions*<hr> Functions with unlimited varaibles
