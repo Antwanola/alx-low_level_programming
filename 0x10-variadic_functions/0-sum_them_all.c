723286634e565f35edca6a840e466b78bfb879ac
antwanola <antwanola29@gmail.com>
2023-08-17 11:32:11 +0000

    Variadic function count

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,12 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_list vars;
 	int i = 0, sum = 0;
-	
+
 	if (n == 0)
 		return (0);
-
-	va_start (vars, n);
-
+	va_start(vars, n);
 	while (i < n)
 	{
 		sum += va_arg(vars, int);
