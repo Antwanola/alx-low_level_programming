17f805955655b1e5e5684c864b62f2bbe6c6ea00
antwanola <antwanola29@gmail.com>
2023-08-20 11:38:55 +0000

    finding int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index- finding index
+ * @array: array input
+ * @size: size of an array
+ * @cmp: function pointer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 ||!array || !cmp)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]) != 0)
+			return (i);
+	return (-1);
+}
