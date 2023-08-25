98aba6fa682753ba5b899c48222203551c8ee82e
antwanola <antwanola29@gmail.com>
2023-08-25 21:07:37 +0000

    solution to the 0 linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,15 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_list- list of struct
+ * @h: pointer to the defined struct
+ * Return: Returned size_t
+ */
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0`;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
@@ -14,9 +19,10 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%lu] %s", count, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		count++;
 	}
+	return (count);
 }
