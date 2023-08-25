65a545631651025bed89c83946124de030178868
antwanola <antwanola29@gmail.com>
2023-08-25 22:00:05 +0000

    Linked list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len- returns the len of linked list
+ * @h: the struxted list who is now a type
+ * Return: return size_t
+ */
+
+size_t list_len(const list_t *h)
+{
+	int len = 0;
+ 
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
