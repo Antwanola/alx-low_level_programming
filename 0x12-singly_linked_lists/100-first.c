d7cfc0ab188f8d5e8da2b6771a572a3abad9ada6
antwanola <antwanola29@gmail.com>
2023-08-26 13:08:08 +0000

    using attribute func to print before main func runs

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * print_before_main- this function
+ * prints things before main
+ * Return: void
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
+}
