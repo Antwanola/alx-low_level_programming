c5266612f93d298dd93786315ec7611b75253066
antwanola <antwanola29@gmail.com>
2023-08-26 00:18:14 +0000

    add node to header

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,7 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (!str)
 		return (NULL);
 
-	new_node = (list_t *)malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 	return (NULL);
 	new_node->str = strdup(str);
@@ -28,7 +28,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new_node->len = strlen(new_node->str);
-
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
