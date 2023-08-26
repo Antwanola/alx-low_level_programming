9cae0800fe9e105b3155feaecce101d80f0dafa6
antwanola <antwanola29@gmail.com>
2023-08-26 00:33:43 +0000

    add node to header

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,25 +13,22 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = NULL;
-	unsigned int i;
-
-	if (!str)
-		return (NULL);
+	size_t i;
 
 	new_node = malloc(sizeof(list_t));
-	if (!new_node)
-	return (NULL);
-	new_node->str = strdup(str);
-	if (!new_node->str)
+	if (new_node == NULL)
 	{
-		free(new_node);
 		return (NULL);
 	}
-	for (i = 0; str[i] != '\0'; i++)
+
+	new_node->str = strdup(str);
+
+	for (i = 0; str[i]; i++)
 		;
+
 	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
-	return (*head);
 
+	return (*head);
 }
