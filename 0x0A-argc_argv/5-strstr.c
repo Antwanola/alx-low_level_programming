8944e8a33e9c42b99dfa11e2bd4ef469976c7563
antwanola <antwanola29@gmail.com>
2023-08-28 00:08:53 +0100

    the strings

---
+++ 0x0A-argc_argv/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * _strstr- the string func
+  * @haystack: the string to be compared
+  * @needle: the string critarea
+  * Return: string
+  */
+
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
+	}
+	return (0);
+}
