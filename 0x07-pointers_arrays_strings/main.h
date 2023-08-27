1f7d070d87546100048f853f7cbdce729a85bde9
antwanola <antwanola29@gmail.com>
2023-08-28 00:21:50 +0100

    The chess master

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,6 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+void print_chessboard(char (*a)[8]);
 
 #endif
