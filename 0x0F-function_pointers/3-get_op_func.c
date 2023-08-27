2ede1c10b1375026a5e97c6913e77ae86bade031
antwanola <antwanola29@gmail.com>
2023-08-27 10:15:35 +0000

    The argv math operator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,11 +20,11 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (i < 10)
 	{
 		if (s[0] == ops->op[i])
 			break;
 		i++;
 	}
-	return (ops[i - 1].f);
+	return (ops[i / 2].f);
 }
