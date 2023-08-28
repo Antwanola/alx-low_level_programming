aee071f639fdaa8c18104a1d26fdfbee8a671f76
antwanola <antwanola29@gmail.com>
2023-08-28 21:55:24 +0100

    freed dogs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,6 +4,44 @@
 #include "dog.h"
 
 /**
+*_strcopy - string copier
+*@dest: pointer for dest str
+*@src: pointer storing string to copy
+*Return: copied
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int index = 0;
+
+for (; src[index] ; index++)
+dest[index] = src[index];
+
+dest[index] = '\0';
+return (dest);
+}
+
+
+/**
+  * _strlen- stringlength
+  * @s: string
+  * Return: int
+  */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
  * new_dog-The new dog typedef func
  * @name: name
  * @age: age
@@ -14,25 +52,31 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog = NULL;
+	int len1 = 0, len2 = 0;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
 
 	newDog = (dog_t *)malloc(sizeof(dog_t));
 
 	if (!newDog)
 		return (NULL);
-	newDog->name = strdup(name);
+	newDog->name = malloc(sizeof(char) * (len1 + 1));
 	if (!newDog->name)
 	{
 		free(newDog);
 		return (NULL);
 	}
 
-	newDog->owner = strdup(owner);
+	newDog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (!newDog->owner)
 	{
 		free(newDog->name);
 		free(newDog);
 		return (NULL);
 	}
+	_strcpy(newDog->name, name);
+	_strcpy(newDog->owner, owner);
 	newDog->age = age;
 	return (newDog);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -36,5 +36,5 @@ typedef struct dog_t
 } dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
-
+void free_dog(dog_t *d);
 #endif
