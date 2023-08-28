25ebe26b350ae4b45d94d8f6906ff873286a7fc7
antwanola <antwanola29@gmail.com>
2023-08-28 15:25:10 +0100

    freeing list and setting head

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+  * free_listint2- free lists
+  * @head: te list head
+  * Return: void
+  */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = NULL;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
