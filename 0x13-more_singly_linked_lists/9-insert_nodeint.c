7aa2711cb15d0f06f2ad190842c1b6d262fa251a
antwanola <antwanola29@gmail.com>
2023-08-29 11:03:48 +0100

    Inser node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,6 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_list->next = *head;
 		*head = new_list;
+		return (new_list);
 	}
 
 	for (; temp && i < idx; i++)
