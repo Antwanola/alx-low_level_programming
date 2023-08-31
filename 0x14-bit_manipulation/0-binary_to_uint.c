cd6e028edd8d4226fb5414d4180f911cd397b499
antwanola <antwanola29@gmail.com>
2023-09-01 00:32:16 +0100

    The bin converter

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 
 	while (b[i] != '\0')
 	{
-		if (b[i] == '0' || b[i] == '1' )
+		if (b[i] == '0' || b[i] == '1')
 		{
 			result = result << 1;
 			result |= b[i] - '0';
