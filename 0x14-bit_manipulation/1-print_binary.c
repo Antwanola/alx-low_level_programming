0c73366cae3692ee68bdba7b2bbd4f2cf52e1581
antwanola <antwanola29@gmail.com>
2023-09-01 01:35:20 +0100

    print bin from decimals

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_binary- print bin numbers
+  * @n: the number in decimal
+  * Return: void
+  */
+
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
