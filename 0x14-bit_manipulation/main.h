bd985512f6c481ab6a7fd78e988dcf3a5dc1e2ae
antwanola <antwanola29@gmail.com>
2023-09-01 01:41:29 +0100

    set index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,8 +4,8 @@
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
-
-
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 
 
