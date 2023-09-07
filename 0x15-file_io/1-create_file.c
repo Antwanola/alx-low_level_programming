b95692f8865e05c95093348453d85afbbb79aade
antwanola <antwanola29@gmail.com>
2023-09-07 23:10:59 +0100

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,18 +21,21 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	while (text_content[i] != '\0')
-		i++;
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	create_file = write(fd, text_content, i);
-	if (create_file == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		while (text_content[i] != '\0')
+			i++;
+		create_file = write(fd, text_content, i);
+		if (create_file == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
