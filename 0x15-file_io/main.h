bd979803ba072fe590c35143cbd9581345afbc16
antwanola <antwanola29@gmail.com>
2023-10-04 23:43:26 +0100

    Create files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 
 #endif
