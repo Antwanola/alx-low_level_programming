52379b4f3a433c129eff8134d63bb207419e4a43
antwanola <antwanola29@gmail.com>
2023-12-14 21:05:02 +0100

    Clear the bit to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * clear_bit-clears the bit
+ * @n: the bit
+ * @index: the index
+ * Return: integer
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
