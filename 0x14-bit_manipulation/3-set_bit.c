3a478b373f3b8686c3c119721cf32cb690198ed9
antwanola <antwanola29@gmail.com>
2023-12-14 20:50:20 +0100

    set the bit at index to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,14 +11,11 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int i;
 
 	if (index >= (sizeof(unsigned long int) * 8))
 	{
 		return (-1);
 	}
-	i = 1;
-	i = i << index;
-	*n = ((*n) | i);
+	*n |= (1 << index);
 	return (1);
 }
