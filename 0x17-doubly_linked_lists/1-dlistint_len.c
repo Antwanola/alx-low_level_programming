bfb4a4bb02330add17e242f217d5aadb29b8187a
antwanola <antwanola29@gmail.com>
2024-01-27 10:33:11 +0100

    The doubly linked list count

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,14 @@
 #include "lists.h"
-
+/**
+ * dlistint_len - returns count of all element
+ * @h: the list
+ * Return: returns the number of elem in list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		h = h->next;
 		i++;
